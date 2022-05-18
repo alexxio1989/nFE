c96a10c816b23c0c7959c027def25bd0ee0b71ad
alexxio1989 <alessiopinna.elis@gmail.com>
2022-05-18 16:50:29 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ApplicationRef, ComponentRef, DoBootstrap, NgModule, NgZone } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,31 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap{
+
+  constructor(private ngZone: NgZone){}
+
+  ngDoBootstrap(appRef: ApplicationRef): void {
+
+    const zone = this.ngZone;
+    let rootRef : ComponentRef<AppComponent>;
+    
+    window['startApp'] = () => {
+      zone.run(() => {
+        rootRef = appRef.bootstrap(AppComponent)
+      })
+    }
+
+    window['killApp'] = () => {
+      if(rootRef){
+        zone.run(() => {
+          rootRef.destroy();
+          rootRef = null;
+        })
+      }
+    }
+
+  } 
+}
